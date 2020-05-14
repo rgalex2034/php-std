bff246c02141d7167f253a94f5c0fb0ccf9e2e58
Alex Ruiz <alex.ruiz@carethy.net>
2020-05-14 18:29:30 +0200

    Added map and filter implementation.

--- src/StdArray.php
+++ src/StdArray.php
@@ -1,7 +1,7 @@
 <?php
 namespace ARG\Std;
 
-class StdArray extends Type\Base{
+class StdArray extends Type\Base implements \ArrayAccess, \Countable{
 
     private $array;
 
@@ -10,11 +10,55 @@ class StdArray extends Type\Base{
     }
 
     public static function from($array){
-        return self::create(...$array);
+        $instance = self::create();
+        foreach($array as $key => $value){
+            $instance[$key] = $value;
+        }
+        return $instance;
     }
 
     public function original(){
         return $this->array;
+    }
+
+    public static function applyMap(array $array, callable $callable, ?array ...$arrays){
+        return $arrays
+            ? array_map($callable, $array, ...$arrays)
+            : array_map($callable, $array);
+    }
+
+    public function map(callable $callable, ?array ...$arrays){
+        $new_array = self::applyMap($this->array, $callable, ...$arrays);
+        return self::from($new_array);
+    }
+
+    public static function applyFilter(array $array, callable $callable, int $flag = 0){
+        return array_filter($array, $callable, $flag);
+    }
+
+    public function filter(callable $callable, int $flag = 0){
+        $new_array = array_filter($this->array, $callable, $flag);
+        return self::from($new_array);
+    }
+
+    public function offsetExists($offset){
+        return isset($this->array[$offset]);
+    }
+
+    public function offsetGet($offset){
+        return $this->array[$offset];
+    }
+
+    public function offsetSet($offset, $value){
+        $this->array[$offset] = $value;
+    }
+
+    public function offsetUnset($offset){
+        unset($this->array[$offset]);
+    }
+
+    public function count(){
+        return count($this->array);
     }
 
 }
--- tests/StdArrayTest.php
+++ tests/StdArrayTest.php
@@ -1,16 +1,24 @@
 <?php
 require_once __DIR__."/../vendor/autoload.php";
 use PHPUnit\Framework\TestCase;
+use ARG\Std\StdArray;
 
 final class StdArrayTest extends TestCase{
+
+    private $array_nums;
+
+    public function __construct(){
+        parent::__construct();
+        $this->array_nums = StdArray::create(1,2,3,4,5);
+    }
 
     /**
      * @test
      */
     public function from(){
         $original = [1,2,3,4];
-        $arr = ARG\Std\StdArray::from($original);
-        $this->assertInstanceOf(ARG\Std\StdArray::class, $arr);
+        $arr = StdArray::from($original);
+        $this->assertInstanceOf(StdArray::class, $arr);
         $this->assertEquals($original, $arr->original());
     }
 
@@ -18,9 +26,43 @@ final class StdArrayTest extends TestCase{
      * @test
      */
     public function create(){
-        $arr = ARG\Std\StdArray::create(1,2,3,4);
-        $this->assertInstanceOf(ARG\Std\StdArray::class, $arr);
+        $arr = StdArray::create(1,2,3,4);
+        $this->assertInstanceOf(StdArray::class, $arr);
         $this->assertEquals([1,2,3,4], $arr->original());
+    }
+
+    /**
+     * @test
+     */
+    public function map(){
+
+        $original = $this->array_nums->original();
+        $double = function($x){
+            return $x*2;
+        };
+
+        $normal_map = array_map($double, $original);
+        $static_map = StdArray::applyMap($original, $double);
+        $method_map = $this->array_nums->map($double);
+        $this->assertEquals($normal_map, $static_map);
+        $this->assertEquals($normal_map, $method_map->original());
+    }
+
+    /**
+     * @test
+     */
+    public function filter(){
+
+        $original = $this->array_nums->original();
+        $greater_than_2 = function($x){
+            return $x > 2;
+        };
+
+        $normal_filter = array_filter($original, $greater_than_2);
+        $static_filter = StdArray::applyFilter($original, $greater_than_2);
+        $method_filter = $this->array_nums->filter($greater_than_2);
+        $this->assertEquals($normal_filter, $static_filter);
+        $this->assertEquals($normal_filter, $method_filter->original());
     }
 
 }
